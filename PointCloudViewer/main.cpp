8c790b4648bc63eb9c563d2658b49c404e90b147
Arata Jingu <guujin0120@gmail.com>
2019-04-09 02:07:35 +0900

    Add Viz

--- PointCloudViewer/main.cpp
+++ PointCloudViewer/main.cpp
@@ -1,15 +1,39 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/viz.hpp>
 
 using namespace cv;
 using namespace std;
 
+static void help()
+{
+	cout
+		<< "--------------------------------------------------------------------------" << endl
+		<< "This program shows how to launch a 3D visualization window. You can stop event loop to continue executing. "
+		<< "You can access the same window via its name. You can run event loop for a given period of time. " << endl
+		<< "Usage:" << endl
+		<< "./launching_viz" << endl
+		<< endl;
+}
+
 int main()
 {
-	Mat image = Mat::zeros(100, 100, CV_8UC3);
-	imshow("", image);
-	cout << "ok" << endl;
-	waitKey();
+	help();
+	viz::Viz3d myWindow("Viz Demo");
+
+	myWindow.spin();
+
+	cout << "First event loop is over" << endl;
+	viz::Viz3d sameWindow = viz::getWindowByName("Viz Demo");
+	sameWindow.spin();
+	cout << "Second event loop is over" << endl;
+	sameWindow.spinOnce(1, true);
+	while (!sameWindow.wasStopped())
+	{
+		sameWindow.spinOnce(1, true);
+	}
+	cout << "Last event loop is over" << endl;
 
 	return 0;
 }
