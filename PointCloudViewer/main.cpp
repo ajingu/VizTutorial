7e0095deb19b0a3d42328a1867fd640f61ff493c
Arata Jingu <guujin0120@gmail.com>
2019-04-09 23:02:38 +0900

    Add one point cloud to viz scene

--- PointCloudViewer/main.cpp
+++ PointCloudViewer/main.cpp
@@ -6,60 +6,61 @@
 using namespace cv;
 using namespace std;
 
-static void help()
-{
-	cout
-		<< "--------------------------------------------------------------------------" << endl
-		<< "This program shows how to use makeTransformToGlobal() to compute required pose,"
-		<< "how to use makeCameraPose and Viz3d::setViewerPose. You can observe the scene "
-		<< "from camera point of view (C) or global point of view (G)" << endl
-		<< "Usage:" << endl
-		<< "./main [ G | C ]" << endl
-		<< endl;
-}
-
-
-static Mat cvcloud_load()
-{
-	Mat cloud(1, 1889, CV_32FC3);
-
-	Point3f* data = cloud.ptr<cv::Point3f>();
-	
-	for (size_t i = 0; i < 1889; ++i)
-	{
-		data[i].x = 0;
-		data[i].y = i * 0.01;
-		data[i].z = 0;
-	}
-	
-	return cloud;
-}
-
+#define MAX_NUM 3
+#define DELTA_TRANSLATION_X 0.005
 
 int main()
 {
-	help();
-	
-	bool camera_pov = false;
+
+	float f = 364, cx = 256.4, cy = 202;
+
 	viz::Viz3d myWindow("Coordinate Frame");
-	myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
-	Vec3f cam_pos(3.0f, 3.0f, 3.0f), cam_focal_point(3.0f, 3.0f, 2.0f), cam_y_dir(-1.0f, 0.0f, 0.0f);
-	Affine3f cam_pose = viz::makeCameraPose(cam_pos, cam_focal_point, cam_y_dir);
-	Affine3f transform = viz::makeTransformToGlobal(Vec3f(0.0f, -1.0f, 0.0f), Vec3f(-1.0f, 0.0f, 0.0f), Vec3f(0.0f, 0.0f, -1.0f), cam_pos);//Œ´“_‚ðŠî€‚É‚µ‚½ƒJƒƒ‰‚ÌAffine•ÏŠ·
-	Mat bunny_cloud = cvcloud_load();
-	viz::WCloud cloud_widget(bunny_cloud, viz::Color::green());
-	Affine3f cloud_pose = Affine3f().translate(Vec3f(0.0f, 0.0f, 3.0f));
-	Affine3f cloud_pose_global = transform * cloud_pose;
-	if (!camera_pov)
+	//myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
+
+	for (int i = 1; i < MAX_NUM + 1; i++)
 	{
-		viz::WCameraPosition cpw(0.5); // Coordinate axes
-		viz::WCameraPosition cpw_frustum(Vec2f(0.889484, 0.523599)); // Camera frustum
-		myWindow.showWidget("CPW", cpw, cam_pose);
-		myWindow.showWidget("CPW_FRUSTUM", cpw_frustum, cam_pose);
+		string src_name = "./data/depth";
+		if (i < 10)
+		{
+			src_name += "0";
+		}
+		src_name += to_string(i) + ".png";
+
+		Mat src, dst;
+		src = imread(src_name, IMREAD_UNCHANGED);
+		src.convertTo(dst, CV_32FC1, 1 / 5000.0f);
+
+		Mat cloud(1, dst.rows * dst.cols, CV_32FC3);
+		Point3f* data = cloud.ptr<Point3f>();
+		for (int row = 0; row < dst.rows; row++)
+		{
+			for (int col = 0; col < dst.cols; col++)
+			{
+				float depth = dst.at<float>(row, col);
+
+				int idx = row * dst.cols + col;
+				data[idx].x = (col - cx) / f * depth;
+				data[idx].y = (row - cy) / f * depth;
+				data[idx].z = depth;
+			}
+		}
+
+		Vec3f cam_pos(0.0f + DELTA_TRANSLATION_X * (i-1), 0.0f, 0.0f), cam_focal_point(0.0f + DELTA_TRANSLATION_X * (i-1), 0.0f, 0.01f), cam_y_dir(0.0f, -1.0f, 0.0f);
+		Affine3f cam_pose = viz::makeCameraPose(cam_pos, cam_focal_point, cam_y_dir);
+		Affine3f cam_transform_matrix = viz::makeTransformToGlobal(Vec3f(-1.0f, 0.0f, 0.0f), Vec3f(0.0f, -1.0f, 0.0f), Vec3f(0.0f, 0.0f, 1.0f), cam_pos);
+		Affine3f cloud_pose_camera = Affine3f().translate(cam_pos);
+		Affine3f cloud_pose_global = cam_transform_matrix * cloud_pose_camera;
+
+		viz::WCloud cloud_widget(cloud, viz::Color::white());
+		myWindow.showWidget("cloud" + to_string(i), cloud_widget, cloud_pose_global);
+
+		viz::WCameraPosition cpw(0.1); // Coordinate axes
+		viz::WCameraPosition cpw_frustum(Vec2f(0.8, 0.5), 0.1); // Camera frustum
+		myWindow.showWidget("CPW" + to_string(i), cpw, cam_pose);
+		myWindow.showWidget("CPW_FRUSTUM" + to_string(i), cpw_frustum, cam_pose);
 	}
-	myWindow.showWidget("bunny", cloud_widget, cloud_pose_global);
-	if (camera_pov)
-		myWindow.setViewerPose(cam_pose);
+	
+
 	myWindow.spin();
 
 	return 0;
